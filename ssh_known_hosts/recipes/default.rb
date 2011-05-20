48a651631e07487862b274aa03f6cf2628c1334b
Nuo Yan <nuo@opscode.com>
2011-05-20 12:09:23 -0700

    further fix COOK-571

--- ssh_known_hosts/recipes/default.rb
+++ ssh_known_hosts/recipes/default.rb
@@ -14,12 +14,26 @@ search(:node, "*:*") do |z|
     z["keys"] = node["keys"]
   end
 
+  begin
+    if z['keys']['ssh']['host_dsa_public'].nil?
+      Chef::Log.warn("ssh_known_hosts: Attribute ['keys']['ssh']['host_dsa_public'] does not exist on node #{z.name}. Skipping node.")
+      next
+    elsif z['keys']['ssh']['host_rsa_public'].nil?
+      Chef::Log.warn("ssh_known_hosts: Attribute ['keys']['ssh']['host_rsa_public'] does not exist on node #{z.name}. Skipping node.")
+      next
+    end
+  rescue
+    Chef::Log.warn("ssh_known_hosts: Attribute ['keys']['ssh'] does not exist on node #{z.name}. Skipping node.")
+    next
+  end
+
   # Skip the node if it doesn't have one or more of these attributes
   if z["hostname"].nil? || z["ipaddress"].nil? || z["keys"].nil?
     Chef::Log.warn("Could not find one or more of these attributes on node #{z.name}: hostname, ipaddress, keys. Skipping node.")
   else
     nodes << z
   end
+
 end
 
 template "/etc/ssh/ssh_known_hosts" do
